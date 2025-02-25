751de919904f6a1fb17f778dca81dd7ee0e5d9b9
Luan Vieira <luanvwittchind@gmail.com>
2025-02-25 14:05:59 -0300

    Criando o hero e imortando a font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-//Exportção do gulp e do sass
+// Importação de plugins
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+// Função para compilar o Sass
 function styles() { 
-    return gulp.src('./src/styles/*.scss')  // 1. Seleciona os arquivos Sass
-        .pipe(sass({ outputStyle: 'compressed' })) // 2. Compila o Sass e trata erros
-        .pipe(gulp.dest('./dist/css/')); // 3. Salva os arquivos CSS na pasta de destino
+    return gulp.src('./src/styles/*.scss')  
+        .pipe(sass({ outputStyle: 'compressed' })) // Compila e comprime o Sass
+        .pipe(gulp.dest('./dist/css/')); // Salva o CSS gerado
 }
-// Exporta a função como tarefa padrão do Gulp
-exports.default = styles;
+
+// Função para otimizar as imagens
+function images() { 
+    return gulp.src('./src/images/**/*')  // Ajustando para capturar imagens
+        .pipe(imagemin()) // Minifica as imagens
+        .pipe(gulp.dest('./dist/images/'));  // Salva as imagens otimizadas na pasta dist/images
+}
+
+// Exportando as funções para que o Gulp execute as tarefas
+exports.default = gulp.parallel(styles, images);  // Executa as tarefas em sequência (styles depois images)
+
+// Função para observar mudanças no Sass
 exports.watch = function () {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-}
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));  // Observa mudanças e executa a tarefa styles
+};
